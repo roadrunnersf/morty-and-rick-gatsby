fe9036554aff39226c5c705d56392899da37ef5f
Sam Finch <roadrunnersf@gmail.com>
2019-08-09 17:29:30 +0100

    create template for single characters page

---
+++ src/templates/CharactersSingle.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react"
+import { Link } from "gatsby"
+import { Helmet } from "react-helmet"
+
+import CharactersViewBox from "./../Components/Characters/CharactersViewBox"
+import NavBar from "./../Components/Layout/NavBar.js"
+
+const CharactersSingle = props => {
+  const character = props.pageContext
+  return (
+    <div>
+      <Helmet>
+        <title>{character.name && character.name}</title>
+      </Helmet>
+      <NavBar />
+      <Link to={`/characters/${parseInt(character.id, 10) + 1}`}>
+        {" "}
+        <CharactersViewBox character={character} width={300} height={520} />
+      </Link>{" "}
+    </div>
+  )
+}
+
+export default CharactersSingle
