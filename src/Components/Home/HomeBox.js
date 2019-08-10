944d5d9fb84ff86102921afee2c3449591332fe4
Sam Finch <roadrunnersf@gmail.com>
2019-08-10 14:50:54 +0100

    add container around jumbotron
    extra padding looks nicer

--- src/Components/Home/HomeBox.js
+++ src/Components/Home/HomeBox.js
@@ -1,24 +1,25 @@
 import React from "react"
 import { Link } from "gatsby"
-import { Jumbotron, Button } from "reactstrap"
+import { Jumbotron, Button, Container } from "reactstrap"
 
 const HomeBox = ({ characters }) => {
-  console.log("hb", characters)
   return (
-    <Jumbotron>
-      <h1 className="display-3">Welcome to Sam's Rick & Morty</h1>
-      <p className="lead">This is a brief explanation of the site</p>
-      <hr className="my-2" />
-      <p>Click below to go to the characters page </p>
-      <p className="lead">
-        <Link to="/characters/">
-          <Button color="primary">Characters</Button>
-        </Link>
-      </p>
-      {characters.map(character => (
-        <p>{character.name}</p>
-      ))}
-    </Jumbotron>
+    <Container>
+      <Jumbotron>
+        <h1 className="display-3">Welcome to Sam's Rick & Morty</h1>
+        <p className="lead">This is a brief explanation of the site</p>
+        <hr className="my-2" />
+        <p>Click below to go to the characters page </p>
+        <p className="lead">
+          <Link to="/characters/">
+            <Button color="primary">Characters</Button>
+          </Link>
+        </p>
+        {characters.map(character => (
+          <p>{character.name}</p>
+        ))}
+      </Jumbotron>
+    </Container>
   )
 }
 
