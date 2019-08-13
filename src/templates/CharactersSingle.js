383bf3c99fa95bc07bffa58b7c78c1d774cc7f4b
Sam Finch <roadrunnersf@gmail.com>
2019-08-13 17:43:45 +0100

    Add Layout component
    Navbar and page Helmets are now in a single layout component that uses 
    {children}. This makes other pages look a bit cleaner.

--- src/templates/CharactersSingle.js
+++ src/templates/CharactersSingle.js
@@ -3,23 +3,19 @@ import { Helmet } from 'react-helmet'
 import 'bootstrap/dist/css/bootstrap.css'
 
 import CharactersViewCard from './../Components/Characters/CharactersViewCard'
-import NavBar from './../Components/Layout/NavBar.js'
+import Layout from './../Components/Layout'
 
 const CharactersSingle = props => {
 	const character = props.pageContext
 	return (
-		<>
-			<Helmet>
-				<title>{character.name && character.name}</title>
-			</Helmet>
-			<NavBar />
+		<Layout>
 			<div style={{ width: 250 }}>
 				<CharactersViewCard
 					character={character}
 					linkURL={`/characters/${parseInt(character.id, 10) + 1}`}
 				/>
 			</div>
-		</>
+		</Layout>
 	)
 }
 
