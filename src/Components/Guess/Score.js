c3af07329ee8a385fac46991ad58927c2e1cca18
Sam Finch <roadrunnersf@gmail.com>
2019-08-15 22:03:37 +0100

    layout to fit on laptop

--- src/Components/Guess/Score.js
+++ src/Components/Guess/Score.js
@@ -3,15 +3,9 @@ import {Progress} from 'reactstrap'
 
 const Score = ({pics, max, score, wrong}) => {
 	return (
-		<div>
-			<div className="text-center" style={{paddingTop: '10px'}}>
-				{score + wrong} of {max}
-			</div>
-
-			<Progress animated multi max={max} style={{height: 40}}>
-				<Progress bar animated value={score + wrong} max={max}></Progress>
-			</Progress>
-		</div>
+		<Progress animated multi max={max} style={{height: 40}}>
+			<Progress bar animated value={score + wrong} max={max}></Progress>
+		</Progress>
 	)
 }
 export default Score
--- src/Components/Layout/NavigationBar.js
+++ src/Components/Layout/NavigationBar.js
@@ -4,7 +4,7 @@ import {Navbar, NavbarBrand, Nav, NavItem, NavLink} from 'reactstrap'
 
 const NavigationBar = () => (
 	<Navbar style={{marginBottom: '8px'}} color="dark" dark expand="xs">
-		<NavbarBrand tag="div">
+		<NavbarBrand style={{padding: '0px'}} tag="div">
 			<Link style={{color: '#999999'}} to="/">
 				{String.raw`<RickAndMorty />`}
 			</Link>
@@ -13,14 +13,14 @@ const NavigationBar = () => (
 		<Nav className="ml-auto" navbar>
 			<NavItem>
 				<Link to="/characters">
-					<NavLink style={{padding: '6px 20px'}} tag="div">
+					<NavLink style={{padding: '2px 20px'}} tag="div">
 						Characters
 					</NavLink>
 				</Link>
 			</NavItem>
 			<NavItem>
 				<Link to="/guess">
-					<NavLink style={{padding: '6px 20px'}} tag="div">
+					<NavLink style={{padding: '2px 20px'}} tag="div">
 						Guessing Game
 					</NavLink>
 				</Link>
