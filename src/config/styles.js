6725bbd05ed857f0089a5e7997772f4e0a06bdf3
Sam Finch <roadrunnersf@gmail.com>
2019-09-02 21:36:02 +0100

    added colors

--- src/config/styles.js
+++ src/config/styles.js
@@ -1,6 +1,9 @@
 export const BLUE = `rgba(32, 130, 239, 1)` //PANTONE 18-427
-export const BLUE_DARKER = `rgba(26, 104, 172, 1)`
+export const BLUE_DARKER = `rgba(26, 104, 172, 1)` //PANTONE 18-427 +20% darker
 export const BLUE_LIGHTER = `rgba(38, 156, 287, 1)` //PANTONE 18-427 +20% lighter
+export const GREEN = `rgba(0,122,83)` //PANTONE 341
+export const RED = `rgba(175,35,28)` //PANTONE 2350
+export const COOL_GREY = `rgba(208, 211, 212, 1)` //PANTONE 427
 
 export const FONT = `'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans',
 		sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol',
