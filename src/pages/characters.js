c27ba12fb5efec1ed7edaf79d6d90cae38c3f764
Sam Finch <roadrunnersf@gmail.com>
2019-09-10 19:23:21 +0100

    create useRandomCharacters custom hook, merge Characters container and view

--- src/pages/characters.js
+++ src/pages/characters.js
@@ -1,23 +1,31 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import {graphql} from 'gatsby'
+import {Grid, Col, Row} from 'react-styled-flexboxgrid'
 
-import CharactersView from './../Components/Characters/CharactersView'
+import {CharacterCard} from '../Components/elements/cards'
 import Layout from './../Components/Layout'
 
-import {randFromList} from './../utils'
+import useRandomCharacters from '../utils/custom-hooks/randomCharacters'
 
 const Characters = ({data}) => {
-	const [characters, setCharacters] = useState([])
-
-	useEffect(() => {
-		setCharacters(randFromList(data.allCharacters.nodes, 20))
-	}, [data.allCharacters.nodes])
+	const characters = useRandomCharacters(data.allCharacters.nodes, 20)
 
 	return (
-		<>
-			<Layout helmet={'Character Viewer'} />
-			<CharactersView characters={characters} />
-		</>
+		<Layout helmet={'Character Viewer'}>
+			<Grid>
+				<Row>
+					{characters.map((character, index) => (
+						<Col xs={6} sm={4} md={4} lg={3} key={character.id}>
+							<CharacterCard
+								key={character.id}
+								character={character}
+								linkURL={`/characters/${character.id}/`}
+							/>
+						</Col>
+					))}
+				</Row>
+			</Grid>
+		</Layout>
 	)
 }
 
