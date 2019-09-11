4ad3fa74755a6419c0d6233597344143fd5c1fad
Sam Finch <roadrunnersf@gmail.com>
2019-09-11 14:47:55 +0100

    refactor guess to add score/wrong in handleDrop rather than useScore
    • this is much less complicated than running a hook every time an item is dropped
    • the hook itself was also quite compliated, relying on cumbersome util functions

--- src/pages/guess/index.js
+++ src/pages/guess/index.js
@@ -8,18 +8,21 @@ import 'bootstrap/dist/css/bootstrap.css'
 import GuessView from './../../Components/Guess/View'
 import Complete from './../../Components/Guess/Complete'
 
-import useScore from '../../utils/hooks/score'
 import ItemTypes from './../../utils/ItemTypes'
-import {shuffle, randFromList, picsComplete} from './../../utils'
+import {shuffle, randFromArray, picsComplete} from './../../utils'
 
 const Guess = ({data}) => {
-	const characters = randFromList(data.allCharacters.nodes, 6)
+	const characters = randFromArray(data.allCharacters.nodes, 6)
+
+	const [score, setScore] = useState(0)
+	const [wrong, setWrong] = useState(0)
+	const [droppedBoxTitles, setDroppedBoxTitles] = useState([])
 
 	const [pics, setPics] = useState(
 		shuffle(characters).map(obj => ({
 			...obj,
 			accepts: ItemTypes.TITLE,
-			lastDroppedItem: null,
+			lastDroppedItem: undefined,
 		}))
 	)
 	const [titles] = useState(
@@ -28,9 +31,6 @@ const Guess = ({data}) => {
 			.sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0))
 			.map(obj => ({...obj, type: ItemTypes.TITLE}))
 	)
-	const [droppedBoxTitles, setDroppedBoxTitles] = useState([])
-
-	const [score, wrong] = useScore(pics)
 
 	const handleDrop = useCallback(
 		(index, item) => {
@@ -38,6 +38,10 @@ const Guess = ({data}) => {
 			setDroppedBoxTitles(
 				update(droppedBoxTitles, name ? {$push: [name]} : {$push: []})
 			)
+			pics[index].name === name
+				? setScore(prevState => prevState + 1)
+				: setWrong(prevState => prevState + 1)
+
 			setPics(
 				update(pics, {
 					[index]: {
@@ -53,7 +57,7 @@ const Guess = ({data}) => {
 
 	return (
 		<DndProvider backend={HTML5Backend}>
-			{picsComplete(pics) ? (
+			{!picsComplete(pics) ? (
 				<GuessView
 					pics={pics}
 					titles={titles}
