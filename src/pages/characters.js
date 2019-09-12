d530ca25929e96d6812d3eb5013c68b61779fc2e
Sam Finch <roadrunnersf@gmail.com>
2019-09-12 17:38:21 +0100

    try to fix styling error

--- src/pages/characters.js
+++ src/pages/characters.js
@@ -1,35 +1,11 @@
 import React from 'react'
 import {graphql} from 'gatsby'
-import {Grid, Col, Row} from 'react-styled-flexboxgrid'
 
-import {CharacterCard} from '../Components/elements/cards'
-import Layout from './../Components/Layout'
+import Characters from '../Components/Characters'
 
-import useRandomCharacters from '../utils/hooks/randomCharacters'
+const CharactersPage = ({data}) => <Characters data={data} />
 
-const Characters = ({data}) => {
-	const characters = useRandomCharacters(data.allCharacters.nodes, 20)
-
-	return (
-		<Layout helmet={'Character Viewer'}>
-			<Grid>
-				<Row>
-					{characters.map((character, index) => (
-						<Col xs={6} sm={4} md={4} lg={3} key={character.id}>
-							<CharacterCard
-								key={character.id}
-								character={character}
-								linkURL={`/characters/${character.id}/`}
-							/>
-						</Col>
-					))}
-				</Row>
-			</Grid>
-		</Layout>
-	)
-}
-
-export default Characters
+export default CharactersPage
 
 export const query = graphql`
 	query CharactersQuery {
