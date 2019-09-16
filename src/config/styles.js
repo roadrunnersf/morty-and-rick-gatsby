da6c232aaa4bcff312f9a3280075758ffcf8d54c
Sam Finch <roadrunnersf@gmail.com>
2019-09-16 15:32:28 +0100

    change FONT, add FAINT_OPACITY and TRANSITION_TIME

--- src/config/styles.js
+++ src/config/styles.js
@@ -6,8 +6,8 @@ export const RED = `rgba(175,35,28)` //PANTONE 2350
 export const COOL_GREY = `rgba(208, 211, 212, 1)` //PANTONE 427
 export const COOL_GREY_LIGHT = `rgba(232,231,234, 1)` //PANTONE 663 XGC
 
-export const FONT = `'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans',
-		sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol',
-		'Noto Color Emoji'`
+export const FONT = `-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"`
 
-export const BORDER_RADIUS = '25px'
+export const FAINT_OPACITY = {LIGHT: '0.8', MEDIUM: '0.6', STRONG: '0.4'}
+
+export const TRANSITION_TIME = '200ms'
