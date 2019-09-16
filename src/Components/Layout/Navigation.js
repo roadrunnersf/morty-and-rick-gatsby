622970a20932bf0c505ac308afb0a93d0d513861
Sam Finch <roadrunnersf@gmail.com>
2019-09-16 15:30:06 +0100

    simplify usage of Nav sub-components by combining styled components into React components

--- src/Components/Layout/Navigation.js
+++ src/Components/Layout/Navigation.js
@@ -1,26 +1,16 @@
 import React from 'react'
-import {Link} from 'gatsby'
 
 import {Nav} from '../elements/layout'
 
 const Navigation = () => {
 	return (
-		<>
-			<Nav.Head />
-			<Nav.Container>
-				<Nav.List>
-					<Nav.Link to="/">
-						<Nav.Item home>Morty & Rick</Nav.Item>
-					</Nav.Link>
-					<Nav.Link to="/characters">
-						<Nav.Item>Characters</Nav.Item>
-					</Nav.Link>
-					<Nav.Link to="/guess">
-						<Nav.Item>Guesser</Nav.Item>
-					</Nav.Link>
-				</Nav.List>
-			</Nav.Container>
-		</>
+		<Nav>
+			<Nav.Item to="/" home>
+				Morty & Rick
+			</Nav.Item>
+			<Nav.Item to="/characters">Characters</Nav.Item>
+			<Nav.Item to="/guess">Guesser</Nav.Item>
+		</Nav>
 	)
 }
 
--- src/Components/elements/layout/Nav.js
+++ src/Components/elements/layout/Nav.js
@@ -1,64 +1,55 @@
+import React from 'react'
 import {Link as GLink} from 'gatsby'
+import styled from 'styled-components'
 
-import styled, {css} from 'styled-components'
 import {FONT, COOL_GREY_LIGHT} from '../../../config/styles'
 
-const listStyle = css`
+const Nav = ({children}) => (
+	<Container>
+		<List>{children}</List>
+	</Container>
+)
+
+const Container = styled.div`
+	position: relative;
+	width: 100%;
+	height: 90px;
+	margin-bottom: 28px;
+	background-color: ${COOL_GREY_LIGHT};
+`
+
+const List = styled.ul`
+	margin: 0;
+	padding: 0;
+	position: absolute;
+	z-index: 100;
+	top: 12px;
+	right: 40px;
+
 	list-style-type: none;
 	text-align: right;
 	font-size: 1rem;
 	line-height: 1.4rem;
 	font-family: ${FONT};
 	font-weight: 400;
+`
+const Item = ({children, ...props}) => {
+	const {to, home} = props
+	return (
+		<Link to={to}>
+			<Text home={home}>{children}</Text>
+		</Link>
+	)
+}
+const Text = styled.li`
 	color: black;
-`
-
-const Head = styled.div`
-	position: relative;
-	top: 0px;
-	right: 0px;
-	box-sizing: content-box;
-	padding: 0;
-	margin: 0px;
-
-	width: calculate(100% + 200px);
-	height: 100px;
-	margin-bottom: 28px;
-	background-color: ${COOL_GREY_LIGHT};
-`
-const Container = styled.nav`
-	position: absolute;
-	z-index: 100;
-	top: 11px;
-	right: 40px;
-`
-const List = styled.ul`
-	${listStyle};
-`
-const Item = styled.li`
-	${listStyle};
 	${props => props.home && `font-weight: 600;`}
-	:hover {
-		font-size: 1.2rem;
-		line-height: 1.7rem;
-		font-weight: calculate(font-weight * 2);
-		transition-property: all;
-		transition-duration: 160ms;
-		transition-timing-function: ease-in-out;
-	}
 `
-/* background-color: black; */
-/* color: ${COOL_GREY_LIGHT}; */
 const Link = styled(GLink)`
 	text-decoration: none;
 	box-shadow: none;
 `
 
-const Nav = {}
-Nav.Head = Head
-Nav.Container = Container
-Nav.List = List
 Nav.Item = Item
-Nav.Link = Link
 
 export {Nav}
