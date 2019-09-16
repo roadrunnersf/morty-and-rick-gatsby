9364a8f5abf228865d5ba91cdeb56997b84cc41f
Sam Finch <roadrunnersf@gmail.com>
2019-09-16 15:27:22 +0100

    add Faint prop to button and use on Title page
    • this keeps the method of changing opacity consistent across pages
    • previously Title was using styles={opacity} to control opacity

--- src/Components/elements/buttons/Button.js
+++ src/Components/elements/buttons/Button.js
@@ -1,5 +1,5 @@
 import styled, {css} from 'styled-components'
-import {FONT, BLUE, BLUE_LIGHTER} from '../../../config/styles'
+import {FONT, BLUE, BLUE_LIGHTER, FAINT_OPACITY} from '../../../config/styles'
 
 const Button = styled.button`
 	font-family: ${FONT};
@@ -38,5 +38,6 @@ const Button = styled.button`
 		}
 		transition: all 160ms ease-in-out
 		`}
+	${props => props.faint && `opacity: ${FAINT_OPACITY.STRONG};`}
 `
 export default Button
