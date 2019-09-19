1fd92fbbb94ab41709c77f62c316e90b144e3b55
Sam Finch <roadrunnersf@gmail.com>
2019-09-19 21:25:08 +0100

    use new limitGrid prop for layout to use useWindow hook

--- src/Components/Layout/index.js
+++ src/Components/Layout/index.js
@@ -2,16 +2,40 @@ import React from 'react'
 import {Helmet} from 'react-helmet'
 import {Grid} from 'react-styled-flexboxgrid'
 
+import useWindowSize from '../../utils/hooks/windowSize'
 import Global from './Global'
 import Nav from './Navigation'
 
-const Layout = ({children, helmet}) => (
+const Layout = ({children, helmet, limitGrid}) => (
 	<>
-		<Global />
-		<Helmet title={helmet} />
-		<Nav />
-		<Grid>{children}</Grid>
+		{limitGrid ? (
+			<LayoutLimitGrid children={children} helmet={helmet} />
+		) : (
+			<LayoutDefault children={children} helmet={helmet} />
+		)}
 	</>
 )
 
 export default Layout
+
+const LayoutDefault = ({children, helmet}) => {
+	const size = 1000
+	return <LayoutContent children={children} helmet={helmet} size={size} />
+}
+const LayoutLimitGrid = ({children, helmet}) => {
+	const size = useWindowSize()
+	return <LayoutContent children={children} helmet={helmet} size={size} />
+}
+
+const LayoutContent = ({size, helmet, children}) => (
+	<>
+		<Global />
+		<Helmet title={helmet} />
+		<Nav />
+		{size.height > 900 ? (
+			<Grid>{children}</Grid>
+		) : (
+			<Grid style={{maxWidth: '1050px'}}>{children}</Grid>
+		)}
+	</>
+)
